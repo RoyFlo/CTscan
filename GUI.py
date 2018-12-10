79f46a4166dbbabb058a2c60b414a4eda5622bd6
RoyFlo <43392885+RoyFlo@users.noreply.github.com>
2018-12-10 14:17:28 -0600

    Working

--- GUI.py
+++ GUI.py
@@ -7,24 +7,41 @@ from skimage.io import imread
 import skimage.transform as skt
 
 window = Tk()
-window.geometry("500x550")
+window.geometry("500x600")
+window.title("CT Scanner")
+
+Label(window, text="IMAGE SELECT").grid(row=0)
+
+this = "Image1.png"
+def iValue(value):
+    global this
+    this = value+".png"
+    print(this)
+
+iList = ["Image1", "Image2", "Image3", "Image4", "Image5"]
+var=StringVar()
+var.set("Image1")
+set1 = OptionMenu(window, var, *iList, command=iValue)
+set1.configure(font=("Ariel"))
+set1.grid(row=1, column=0)
 
 fig = Figure(figsize=(5, 5))
 canvas = FigureCanvasTkAgg(fig, master=window)
 
-
 def plot():
-    image = imread("Image1.png", as_gray=True)
-    image = skt.rescale(image, scale=0.4, mode='reflect', multichannel=False)
+    print("Plotting")
+    print("This = " + this)
+    image = imread(this, as_gray=True)
+    image2 = skt.rescale(image, scale=0.4, mode='reflect', multichannel=False)
 
     a1 = fig.add_subplot(221)
     a2 = fig.add_subplot(222)
 
-    a1.imshow(image, cmap=plt.cm.Greys_r)
+    a1.imshow(image2, cmap=plt.cm.Greys_r)
     a1.set_title("Original")
 
-    theta = np.linspace(0., 180., max(image.shape), endpoint=False)
-    sinogram = skt.radon(image, theta=theta, circle=True)
+    theta = np.linspace(0., 180., max(image2.shape), endpoint=False)
+    sinogram = skt.radon(image2, theta=theta, circle=True)
 
     a2.imshow(sinogram, cmap=plt.cm.Greys_r, extent=(0, 180, 0, sinogram.shape[0]), aspect='auto')
     a2.set_title("Radon transform\n(Sinogram)")
@@ -35,39 +52,23 @@ def plot():
     a4 = fig.add_subplot(224)
 
     reconstruction_fbp = skt.iradon(sinogram, theta=theta, circle=True)
-    error = reconstruction_fbp - image
+    error = reconstruction_fbp - image2
     print('FBP rms reconstruction error: %.3g' % np.sqrt(np.mean(error ** 2)))
     imkwargs = dict(vmin=-0.2, vmax=0.2)
 
     a3.imshow(reconstruction_fbp, cmap=plt.cm.Greys_r)
     a3.set_title('Reconstruction\nFiltered back porjection')
 
-    a4.imshow(reconstruction_fbp - image, cmap=plt.cm.Greys_r, **imkwargs)
+    a4.imshow(reconstruction_fbp - image2, cmap=plt.cm.Greys_r, **imkwargs)
     a4.set_title('Reconstruction error\nFiltered back projection')
 
     fig.tight_layout()
     canvas = FigureCanvasTkAgg(fig, master=window)
-    canvas.get_tk_widget().grid(row=1, columnspan=5)
+    canvas.get_tk_widget().grid(row=2, columnspan=5)
     canvas.draw()
-    print("Plotting")
 
+button1 = Button(window, text="PLOT", command=plot)
+button1.grid(row=1, column=2)
 
-def clear():
-    print("clearing")
-
-
-button1 = Button(window, text="Image 1", command=plot)
-button2 = Button(window, text="Image 2", command=plot)
-button3 = Button(window, text="Image 3", command=plot)
-button4 = Button(window, text="Image 4", command=plot)
-button5 = Button(window, text="Image 5", command=plot)
-button6 = Button(window, text="CLEAR", command=clear)
-
-button1.grid(row=0, column=0)
-button2.grid(row=0, column=1)
-button3.grid(row=0, column=2)
-button4.grid(row=0, column=3)
-button5.grid(row=0, column=4)
-button6.grid(row=5, columnspan=5)
 
 window.mainloop()
