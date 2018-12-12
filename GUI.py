ff8c367a05ef7129c878d6231d4e49d7bf4e919f
Rafael Sanchez <42717360+rasanchez180@users.noreply.github.com>
2018-12-11 23:43:28 -0600

    My final update

--- GUI.py
+++ GUI.py
@@ -1,3 +1,7 @@
+#Rogelio Flores - 125726
+#Rafael Sanchez - 1568678
+
+
 from tkinter import *
 import matplotlib.pyplot as plt
 import numpy as np
@@ -7,15 +11,17 @@ from skimage.io import imread
 import skimage.transform as skt
 
 window = Tk()
-window.geometry("700x800")
+window.geometry("700x820")
 window.title("CT Scanner")
 
-Label(window, text="Select an image from the list below.", font=("Ariel", 12), fg="blue").grid(row=0)
-Label(window, text="Select the scale.", font=("Ariel", 12), fg="blue").grid(row=0, column=1)
-Label(window, text="Then hit Simulate", font=("Ariel", 12), fg="blue").grid(row=0, column=3)
+Label(window, text="1. Select an image from the list below.", font=("Ariel", 12), fg="blue").grid(row=0)
+Label(window, text="2. Select the scale for the output.\n(1/n of original resolution)", font=("Ariel", 12), fg="blue").grid(row=0, column=1)
+Label(window, text="3. Hit Run Simulation", font=("Ariel", 12), fg="blue").grid(row=0, column=3)
 
 this = "Image1.png"
-scale = 1
+scale = 4
+print("Default image is " + this + " and scanner resolution will be \n\t1/" + str(scale) + " the resolution of original.")
+
 def iValue(value):
     global this
     this = value+".png"
@@ -26,12 +32,12 @@ def sValue(value):
     scale = value
     print(scale)
 
-iList = ["SheppLogan_Phantom", "Image1", "Image2", "Image3", "Image4", "Image5", "test2"]
+iList = ["Image1", "Image2", "Image3", "Image4", "Image5", "SheppLogan_Phantom"]
 sList = [1,2,3,4,5,6,7,8,9,10]
 var1=StringVar()
 var1.set("Image1")
 var2=StringVar()
-var2.set("1")
+var2.set(4)
 set1 = OptionMenu(window, var1, *iList, command=iValue)
 set1.configure(font=("Ariel"))
 set1.grid(row=1, column=0)
@@ -43,13 +49,10 @@ fig = Figure(figsize=(7, 7))
 canvas = FigureCanvasTkAgg(fig, master=window)
 
 def plot():
-    resoScale = 2
-    #numofSlices = 180
-
     print("\nSimulating using " + this + " using Filtered Back Projection (FBP) for reconstruction")
     image = imread(this, as_gray=True)
-    image2 = skt.rescale(image, scale=1/resoScale, mode='reflect', multichannel=False)
-    scaledres = int((image.shape[0])/resoScale)
+    image2 = skt.rescale(image, scale=1/scale, mode='reflect', multichannel=False)
+    scaledres = int((image.shape[0])/scale)
     print("Scanner resolution is set to " + str(scaledres) + " x " + str(scaledres))
 
     a1 = fig.add_subplot(221)
@@ -80,7 +83,7 @@ def plot():
 
     error = reconstruction_fbp - image2
     print('FBP rms reconstruction error: %.3g' % np.sqrt(np.mean(error ** 2)))
-    rerror = 'FBP rms reconstruction error: %.3g' % np.sqrt(np.mean(error ** 2))
+    rerror = 'Scanner resolution is set to ' + str(scaledres) + ' x ' + str(scaledres) + '\nFBP rms reconstruction error: %.3g' % np.sqrt(np.mean(error ** 2))
     imkwargs = dict(vmin=-0.2, vmax=0.2)
 
     a3.imshow(reconstruction_fbp, cmap=plt.cm.Greys_r)
@@ -95,7 +98,7 @@ def plot():
     Label(window, text=rerror, font=("Ariel", 10), fg="red").grid(row=3, sticky=SE)
     canvas.draw()
 
-button1 = Button(window, text="Simulate", bg="red", command=plot)
+button1 = Button(window, text="Run Simulation", bg="red", command=plot)
 button1.grid(row=1, column=3)
 
 window.mainloop()
